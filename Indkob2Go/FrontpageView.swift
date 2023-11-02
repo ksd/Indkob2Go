6babe3cb068206fa9326723c2835c92623dcec3f
Kaj Schermer Didriksen <kdidriksen@me.com>
2023-11-02 12:47:01 +0100

    Version 1: Views

---
+++ Indkob2Go/FrontpageView.swift
@@ -0,0 +1,18 @@
+//
+//  FrontpageView.swift
+//  Indkob2Go
+//
+//  Created by ksd on 02/11/2023.
+//
+
+import SwiftUI
+
+struct FrontpageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrontpageView()
+}
---
+++ Indkob2Go/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Indkob2Go
+//
+//  Created by ksd on 02/11/2023.
+//
+
+import Foundation
